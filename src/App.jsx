4e57d5de73c23e8b5437fb28c0e9de610449b5e4
Esma Karaca <killbillbeatrixx@gmail.com>
2024-03-18 22:33:50 +0300

    feat: Install and configure Shadcn UI for React components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+import React from "react";
+import { Button } from "@/components/ui/button";
 
+function App() {
   return (
-      <header className="App-header">
-        Extension
-      </header>
-  )
+    <header>
+      <div>
+        <Button className="m-5">Click me</Button>
+      </div>
+    </header>
+  );
 }
 
-export default App
+export default App;
