cd445510e11419ea26d9101ab9dc3b7d34781eb8
Esma Karaca <killbillbeatrixx@gmail.com>
2024-03-19 16:12:20 +0300

    fix: Adjust build settings and update manifest for Chrome extension.

--- src/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === "getTabs") {
-        chrome.tabs.query({ currentWindow: true }, function (tabs) {
-            sendResponse({ tabs: tabs });
-        });
-        return true;
-    }
-});
