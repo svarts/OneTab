69f34b53deb003de23fed3395d357ecbf2fac994
Esma Karaca <killbillbeatrixx@gmail.com>
2024-03-20 17:45:30 +0300

    feat: Add undo option for cleared saved tabs in Popup.

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import logo from '@assets/img/logo.svg';
+import { useState, useEffect } from 'react';
 import '@pages/popup/Popup.css';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import { Button } from '@/components/ui/button';
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const Popup = () => {
   const [tabs, setTabs] = useState([]);
   const [savedTabs, setSavedTabs] = useState([]);
-  const theme = useStorage(exampleThemeStorage);
 
   useEffect(() => {
     chrome.tabs.query({ currentWindow: true }, function (retrievedTabs) {
@@ -35,33 +32,66 @@ const Popup = () => {
     });
   };
 
+  const clearSavedTabs = () => {
+    chrome.storage.local.remove('savedTabs', () => {
+      setSavedTabs([]);
+    });
+  };
+
+  const openTab = (url) => {
+    chrome.tabs.create({ url });
+  };
+
+  const getFaviconUrl = (url) => {
+    try {
+      const { hostname } = new URL(url);
+      return `https://www.google.com/s2/favicons?sz=64&domain=${hostname}`;
+    } catch {
+      return ''; 
+    }
+  };
+  
+
   return (
-    <div
-      className="App rounded-full"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-        borderRadius: '10px',
-      }}>
-      <header className="App-header" style={{ color: theme === 'light' ? '#000' : '#fff' }}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
-        </p>
-        <Button onClick={saveTabs} className="mb-4 text-center">
-          Save Tabs
-        </Button>
-        <ul>
-          {tabs.map(tab => (
-            <li key={tab.id}>{tab.title}</li>
-          ))}
-        </ul>
-        <h2>Saved Tabs</h2>
-        <ul>
-          {savedTabs.map((url, index) => (
-            <li key={index}>{url}</li>
-          ))}
-        </ul>
-      </header>
+    <div className="flex items-center justify-center bg-neutral-900 text-white">
+      <div className="w-full bg-neutral-900 rounded-lg shadow-xl border border-gray-400 overflow-hidden">
+        <header className="flex justify-between p-4 bg-neutral-800">
+          <h1 className="text-lg font-semibold">OneTab</h1>
+          <Button onClick={saveTabs} className="py-2 px-4 bg-lime-500 hover:bg-lime-400 rounded text-sm text-black font-bold">
+            Save Tabs
+          </Button>
+        </header>
+        <div className="flex p-4">
+          <div className="flex-1 mr-2 w-32">
+            <h2 className="text-md font-semibold mb-2">Open Tabs</h2>
+            <ScrollArea className="rounded bg-neutral-800 whitespace-nowrap">
+              {tabs.map(tab => (
+                <li key={tab.id} className="flex items-center border-b border-neutral-600 last:border-b-0 p-2">
+                  <img src={getFaviconUrl(tab.url)} alt="Favicon" className="w-4 h-4 mr-2" />
+                  <div>{tab.title}</div>
+                </li>
+              ))}
+            </ScrollArea>
+          </div>
+
+          <div className="flex-1 ml-2">
+            <h2 className="text-md font-semibold mb-2">Saved Tabs</h2>
+            <ScrollArea className="rounded bg-neutral-800">
+              {savedTabs.map((url, index) => (
+                <li key={index} className="flex items-center border-b border-neutral-600 last:border-b-0 p-2">
+                  <img src={getFaviconUrl(url)} alt="Favicon" className="w-4 h-4 mr-2" />
+                  <a href="#" onClick={() => openTab(url)} className="max-w-sm">
+                    {url}
+                  </a>
+                </li>
+              ))}
+            </ScrollArea>
+            <Button onClick={clearSavedTabs} className="py-2 px-4 bg-rose-900 hover:bg-rose-950 rounded text-sm text-white font-semibold mt-2">
+              Clear Saved Tabs
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
